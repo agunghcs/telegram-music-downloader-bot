c61784159a5cbf1b014f56986e0afaf09c8e9195
agunghcs <65334552+agunghcs@users.noreply.github.com>
2024-03-23 20:26:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+
 import os
 import youtube_dl
 import telepotpro
@@ -19,7 +20,7 @@ class Music:
         self.user_input = user_input[6:]
 
     def search_music(self, user_input):
-        return VideosSearch(user_input, limit = 1).result()
+        return VideosSearch(user_input, limit=1).result()
 
     def get_link(self, result):
         return result['result'][0]['link']
@@ -49,7 +50,7 @@ class Music:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=True)
 
-        pass
+    pass
 
 class Chat:
     def __init__(self, msg):
@@ -61,28 +62,24 @@ class Chat:
 
         self.messages = {
             'start':'🤖 Hello, '+ self.user_name +'!\n\n'
-                    '📩 Send me:\n\n'
-                    '"*/music* _song name_"  or\n'
-                    '"*/music* _musician name - song name_"\n\n'
-                    'to order some music. 🎶',
+                    '📩 Kirimkan saya:\n\n'
+                    '"*/music* _judul lagu_"  atau\n'
+                    '"*/music* _nama musisi - judul lagu_"\n\n'
+                    'untuk memesan beberapa musik. 🎶', 
 
-            'spotify_input_error':"‼️ *Oops! The bot doesn't support Spotify links!*\n"
-                    'Try: "*/music* _song name_"\n'
-                    'or: "*/music* _musician name - song name_"',
+            'spotify_input_error':"‼️ *Oops! Bot tidak mendukung tautan Spotify!*\n"
+                    'Coba: "*/music* _judul lagu_"\n'
+                    'atau: "*/music* _nama musisi - judul lagu_"',
 
-            'invalid_command':'‼️ *Oops! Invalid command!*\n'
-                    'Try: "*/music* _song name_"\n'
-                    'or: "*/music* _musician name - song name_"',
+            'invalid_command':'‼️ *Oops! Perintah tidak valid!*\n'
+                    'Coba: "*/music* _judul lagu_"\n'
+                    'atau: "*/music* _nama musisi - judul lagu_"',
 
-            'too_long':'‼️ *Oops! Video too long to convert!*\n'
-                    'Order something 30 minutes or less.'
-
-
+            'too_long':'‼️ *Oops! Video terlalu panjang untuk dikonversi!*\n'
+                    'Pesan sesuatu yang berdurasi 30 menit atau kurang.'
         }
 
         self.check_input(self.user_input, msg)
-
-        pass
 
     def send_message(self, content):
         return bot.sendMessage(self.chat_id, content, reply_to_message_id=self.message_id, parse_mode='Markdown')
@@ -92,12 +89,8 @@ class Chat:
         message_id = message['message_id']
         bot.deleteMessage((chat_id, message_id))
 
-        pass
-
     def send_audio(self, file_name):
-        bot.sendAudio(self.chat_id,audio=open(file_name,'rb'), reply_to_message_id=self.message_id)
-
-        pass
+        bot.sendAudio(self.chat_id, audio=open(file_name, 'rb'), reply_to_message_id=self.message_id)
 
     def process_request(self, user_input):
         result = Music.search_music(self, user_input[6:])
@@ -105,7 +98,7 @@ class Chat:
         min_duration, split_count = Music.get_duration(self, result)
 
         if int(min_duration) < 30 and split_count < 3:
-            file_name = Music.get_title(self, result) +' - @TLMusicDownloader_bot '+str(randint(0,999999))+'.mp3'
+            file_name = Music.get_title(self, result) + ' - @TLMusicDownloader_bot ' + str(randint(0, 999999)) + '.mp3'
             file_name = file_name.replace('"', '')
 
             self.send_message(f"🎵 {Music.get_title(self, result)}\n🔗 {Music.get_link(self, result)}")
@@ -116,34 +109,30 @@ class Chat:
             try:
                 self.send_audio(file_name)
                 self.delete_message(downloading_message)
-                self.send_message('✅ Sucess!')
-                print ("\nSucess!\n")
+                self.send_message('✅ Success!')
+                print("\nSucces!\n")
             except:
                 print("\nError")
 
             os.remove(file_name)
-        pass
 
     def check_input(self, user_input, msg):
         if user_input.startswith('/start'):
             self.send_message(self.messages['start'])
 
-        elif user_input.startswith('') and user_input[6:]!='':
+        elif user_input.startswith('') and user_input[6:] != '':
             if 'open.spotify.com' in user_input[6:]:
                 self.send_message(self.messages['spotify_input_error'])
 
             else:
-                #Valid command
+                # Valid command
                 self.process_request(user_input)
 
         else:
-            #Invalid command
+            # Invalid command
             self.send_message(self.messages['invalid_command'])
-
-        pass 
 
 def start_new_chat(msg):
     Process(target=Chat, args=(msg,)).start()
-
 
 bot.message_loop(start_new_chat, run_forever=True)
